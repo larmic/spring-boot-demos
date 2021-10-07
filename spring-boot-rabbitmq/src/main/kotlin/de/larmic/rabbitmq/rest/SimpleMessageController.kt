6543b6be3685a0f1d8c855fc0b6c3b8ce1272418
Lars Michaelis <l.michaelis.ext@ewe.de>
2021-10-07 16:25:52 +0200

    feat: add rabbit docker-compose.yml

--- spring-boot-rabbitmq/src/main/kotlin/de/larmic/rabbitmq/rest/SimpleMessageController.kt
+++ spring-boot-rabbitmq/src/main/kotlin/de/larmic/rabbitmq/rest/SimpleMessageController.kt
@@ -1,23 +1,22 @@
 package de.larmic.rabbitmq.rest
 
 import de.larmic.rabbitmq.properties.RabbitProperties
+import de.larmic.rabbitmq.service.RabbitSender
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
-class SimpleMessageController(private val rabbitProperties: RabbitProperties) {
+class SimpleMessageController(private val rabbitSender: RabbitSender) {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
     @PostMapping("/")
     fun tweet(@RequestBody message: String) {
-        log.info("rabbitProperties.exchangeName '${rabbitProperties.exchangeName}'")
-        log.info("rabbitProperties.inbound.queueName '${rabbitProperties.queueName}'")
-        log.info("rabbitProperties.inbound.routingKey '${rabbitProperties.routingKey}'")
-        log.info("rabbitProperties.outbound.routingKey '${rabbitProperties.routingKey}'")
         log.info("[REST] Message '$message' received")
+        rabbitSender.sendSimpleMessage(UUID.randomUUID(), message)
     }
 
 }

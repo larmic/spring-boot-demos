a6b9df8224d3f3b1dce2f2d3451b7931a2830db4
Lars Michaelis <l.michaelis.ext@ewe.de>
2022-03-29 19:24:08 +0200

    refactor: a bit more kotlin style

--- spring-boot-rest-services-kotlin/src/test/kotlin/de/larmic/rest/rest/TweetControllerTest.kt
+++ spring-boot-rest-services-kotlin/src/test/kotlin/de/larmic/rest/rest/TweetControllerTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.util.*
@@ -21,34 +21,32 @@ internal class TweetControllerTest {
     private lateinit var mockMvc: MockMvc
 
     @Autowired
-    private val inMemoryRepository: InMemoryRepository? = null
+    private lateinit var inMemoryRepository: InMemoryRepository
 
     @BeforeEach
     fun setUp() {
-        inMemoryRepository!!.tweets.clear()
+        inMemoryRepository.tweets.clear()
     }
 
     @Nested
     @DisplayName("tweet() with")
     internal inner class TweetTests {
+
         @Test
-        @DisplayName("body is empty")
-        fun bodyIsEmpty() {
-            mockMvc.perform(MockMvcRequestBuilders.post("/").content(""))
+        fun `body is empty`() {
+            mockMvc.perform(post("/").content(""))
                 .andExpect(status().is4xxClientError)
         }
 
         @Test
-        @DisplayName("body is not set")
-        fun bodyIsNull() {
-            mockMvc.perform(MockMvcRequestBuilders.post("/"))
+        fun `body is not set`() {
+            mockMvc.perform(post("/"))
                 .andExpect(status().is4xxClientError)
         }
 
         @Test
-        @DisplayName("body is set")
-        fun bodyIsNotEmpty() {
-            mockMvc.perform(MockMvcRequestBuilders.post("/").content("first test tweet"))
+        fun `body is set`() {
+            mockMvc.perform(post("/").content("first test tweet"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("message", `is`("first test tweet")))
                 .andExpect(jsonPath("id").isNotEmpty)
@@ -58,19 +56,18 @@ internal class TweetControllerTest {
     @Nested
     @DisplayName("readTweet() with")
     internal inner class ReadTweetTests {
+
         @Test
-        @DisplayName("tweet not exists")
-        fun tweetNotExists() {
-            mockMvc.perform(MockMvcRequestBuilders.get("/not-existing"))
+        fun `tweet not exists`() {
+            mockMvc.perform(get("/not-existing"))
                 .andExpect(status().isNotFound)
                 .andExpect(jsonPath("$").doesNotExist())
         }
 
         @Test
-        @DisplayName("tweet exists")
-        fun tweetExists() {
-            val tweet = addTweetToDatabase("second test tweet")
-            mockMvc.perform(MockMvcRequestBuilders.get("/" + tweet.id))
+        fun `tweet exists`() {
+            val tweet = "second test tweet".wrapInTweet().storeInDatabase()
+            mockMvc.perform(get("/${tweet.id}"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("id", `is`(tweet.id)))
                 .andExpect(jsonPath("message", `is`(tweet.message)))
@@ -80,50 +77,49 @@ internal class TweetControllerTest {
     @Nested
     @DisplayName("readAllTweets() with")
     internal inner class ReadAllTweetsTests {
+
         @Test
-        @DisplayName("tweets are empty")
-        fun tweetsAreEmpty() {
-            mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        fun `tweets are empty`() {
+            mockMvc.perform(get("/"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("$").isEmpty)
         }
 
         @Test
-        @DisplayName("tweets are not empty")
-        fun tweetsAreNotEmpty() {
-            val tweet = addTweetToDatabase("third test tweet")
-            mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        fun `tweets are not empty`() {
+            val tweet = "third test tweet".wrapInTweet().storeInDatabase()
+            mockMvc.perform(get("/"))
                 .andExpect(status().isOk)
-                .andExpect(jsonPath("$[0].id", `is`<String>(tweet.id)))
-                .andExpect(jsonPath("$[0].message", `is`<String>(tweet.message)))
+                .andExpect(jsonPath("$[0].id", `is`(tweet.id)))
+                .andExpect(jsonPath("$[0].message", `is`(tweet.message)))
         }
     }
 
     @Nested
     @DisplayName("updateTweet() with")
     internal inner class UpdateTweetTests {
+
         @Test
-        @DisplayName("tweet not exists")
-        fun tweetsAreEmpty() {
-            mockMvc.perform(MockMvcRequestBuilders.put("/not-existing").content("tweet content changed"))
+        fun `tweet not exists`() {
+            mockMvc.perform(put("/not-existing").content("tweet content changed"))
                 .andExpect(status().isNotFound)
                 .andExpect(jsonPath("$").doesNotExist())
         }
 
         @Test
-        @DisplayName("tweet exists")
-        fun tweetsAreNotEmpty() {
-            val tweet = addTweetToDatabase("fourth test tweet")
-            mockMvc.perform(MockMvcRequestBuilders.put("/" + tweet.id).content("tweet content changed"))
+        fun `tweet exists`() {
+            val tweet = "fourth test tweet".wrapInTweet().storeInDatabase()
+            mockMvc.perform(put("/" + tweet.id).content("tweet content changed"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("id", `is`<String>(tweet.id)))
                 .andExpect(jsonPath("message", `is`("tweet content changed")))
         }
     }
 
-    private fun addTweetToDatabase(tweet: String): TweetDto {
-        val dto = TweetDto(UUID.randomUUID().toString(), tweet)
-        inMemoryRepository!!.tweets[dto.id] = dto
-        return dto
+    private fun String.wrapInTweet() = TweetDto(UUID.randomUUID().toString(), this)
+
+    private fun TweetDto.storeInDatabase(): TweetDto {
+        inMemoryRepository.tweets[this.id] = this
+        return this
     }
 }

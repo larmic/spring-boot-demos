6dc8ac240cb0e19e0a953c206d34d6ff2e073c7d
Lars Michaelis <mail@larmic.de>
2022-04-24 10:21:42 +0200

    feat[#186]: add kotlin dependency and migrate PostgresApplication.java to PostgresApplication.kt

---
+++ spring-boot-postgres-kotlin/src/main/kotlin/de/larmic/properties/PostgresApplication.kt
@@ -0,0 +1,11 @@
+package de.larmic.properties
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PropertiesApplication
+
+fun main(args: Array<String>) {
+    runApplication<PropertiesApplication>(*args)
+}

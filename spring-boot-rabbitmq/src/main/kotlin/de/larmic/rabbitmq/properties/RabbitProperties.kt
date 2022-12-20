de110937a76dd48367394db7f3fe5fc49805223a
Lars Michaelis <mail@larmic.de>
2022-12-20 18:46:45 +0100

    migrate: spring boot 3 -> fixed kotlin rabbit example

--- spring-boot-rabbitmq/src/main/kotlin/de/larmic/rabbitmq/properties/RabbitProperties.kt
+++ spring-boot-rabbitmq/src/main/kotlin/de/larmic/rabbitmq/properties/RabbitProperties.kt
@@ -1,10 +1,8 @@
 package de.larmic.rabbitmq.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
 
-@ConstructorBinding
 @ConfigurationProperties("larmic.amqp")
 @Validated
 class RabbitProperties(val exchangeName: String, val queueName: String, val routingKey: String)

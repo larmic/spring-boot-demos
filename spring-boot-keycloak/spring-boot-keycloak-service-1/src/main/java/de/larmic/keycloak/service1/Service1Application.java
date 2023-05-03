d8e806abf8754d8cd81e7877565434d7c534c7ce
larmic <mail@larmic.de>
2023-05-03 10:55:49 +0200

    feat[keycloak]: bump to spring boot 3

--- spring-boot-keycloak/spring-boot-keycloak-service-1/src/main/java/de/larmic/keycloak/service1/Service1Application.java
+++ spring-boot-keycloak/spring-boot-keycloak-service-1/src/main/java/de/larmic/keycloak/service1/Service1Application.java
@@ -2,6 +2,8 @@ package de.larmic.keycloak.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Service1Application {
@@ -10,4 +12,8 @@ public class Service1Application {
 		SpringApplication.run(Service1Application.class, args);
 	}
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- spring-boot-keycloak/spring-boot-keycloak-service-1/src/main/java/de/larmic/keycloak/service1/security/KeycloakSecurityConfig.java
+++ spring-boot-keycloak/spring-boot-keycloak-service-1/src/main/java/de/larmic/keycloak/service1/security/KeycloakSecurityConfig.java
@@ -1,49 +1,45 @@
 package de.larmic.keycloak.service1.security;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
-class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+class KeycloakSecurityConfig {
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.authorizeRequests()
-            .antMatchers("/unsecure/*").permitAll()
-            .antMatchers("/secure/*").hasRole("user");
-        http.csrf().disable();
-    }
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
 
-    @Autowired
-    public void configureGlobal(final AuthenticationManagerBuilder auth) {
-        final var keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+    KeycloakSecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
     }
 
     @Bean
-    @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Bean
-    public KeycloakConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(it -> {
+            it
+                .requestMatchers("/unsecure/*").permitAll()
+                .requestMatchers("/secure/*").hasRole("user");
+        }).csrf().disable();
+
+        http.oauth2Login()
+            .and()
+            .logout()
+            .addLogoutHandler(keycloakLogoutHandler)
+            .logoutSuccessUrl("/");
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        return http.build();
     }
 }
